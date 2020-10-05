27af7b3733bdb4f76d5a5d0b87dac298b3d92258
Shay <smkelley91@gmail.com>
2020-10-05 00:17:18 -0500

    solution added

--- insertionSort.js
+++ insertionSort.js
@@ -1,14 +1,17 @@
 function insertionSort(arr) {
   // loop over the array starting at index 1 incrementing
-
-  // set the current value to a temp variable
-
-  // nest a loop starting at index 0 but dynmaicly decrementing 
-  // setting a condition if the nested value is greater than the value set the temp variable written above
-
-  // inside the nested loop set the nested value + 1 index to the nested value
-
-  // inside the outer loop set the nested loop's value + 1 index to the temp variable written above
+  for (var i = 1; i < arr.length; i++) {
+    // set the current value to a temp variable
+    let currentVal = arr[i];
+    // nest a loop starting at index 0 but dynmaicly decrementing 
+    // setting a condition if the nested value is greater than the value set the temp variable written above
+    for (var k = i - 1; k >= 0 && arr[k] > currentVal; k--) {
+      // inside the nested loop set the nested value + 1 index to the nested value
+      arr[k + 1] = arr[k];
+    }
+    // inside the outer loop set the nested loop's value + 1 index to the temp variable written above
+    arr[k + 1] = currentVal;
+  }
 }
 
 let arr = [2, 1, 5, 3];
